be82ffbe7a592410373337bccd573efebe11bfe0
Janne <jabbekeipert@gmail.com>
2025-11-27 12:00:06 +0100

    Spring Initializer

---
+++ src/main/java/com/magentamause/cosybackend/CosyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.magentamause.cosybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magentamause/cosybackend/CosyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magentamause.cosybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
