0dff2291568a34db2d88c4b19d68dfc636dfff44
Janne <jabbekeipert@gmail.com>
2025-11-27 12:11:21 +0100

    Add Root Endpoint

--- src/test/java/com/magentamause/cosybackend/CosyBackendApplicationTests.java
+++ src/test/java/com/magentamause/cosybackend/CosyBackendApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CosyBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
