25bda215773969f750c1bbcf730dc95c3a0268f8
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2025-12-01 23:30:24 +0100

    refactor: got rid of login entity and adjusted user entity

--- src/main/java/com/magentamause/cosybackend/entities/UserEntity.java
+++ src/main/java/com/magentamause/cosybackend/entities/UserEntity.java
@@ -2,8 +2,16 @@ package com.magentamause.cosybackend.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.*;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "user_entity")
 public class UserEntity {
     @Id private String uuid;
+    private String username;
+    private String password;
 }
--- src/main/java/com/magentamause/cosybackend/services/JwtService.java
+++ src/main/java/com/magentamause/cosybackend/services/JwtService.java
@@ -1,6 +1,6 @@
 package com.magentamause.cosybackend.services;
 
-import com.magentamause.cosybackend.entities.LoginEntry;
+import com.magentamause.cosybackend.entities.UserEntity;
 import com.magentamause.cosybackend.security.JwtTokenBody;
 import com.magentamause.cosybackend.security.JwtUtils;
 import java.util.Map;
@@ -12,24 +12,28 @@ import org.springframework.stereotype.Service;
 public class JwtService {
     private final JwtUtils jwtUtils;
 
-    public String buildRefreshToken(LoginEntry loginEntry) {
+    public String buildRefreshToken(UserEntity user) {
         Map<String, Object> claims =
                 Map.of(
                         "username",
-                        loginEntry.getUsername(),
+                        user.getUsername(),
                         "tokenType",
                         JwtTokenBody.TokenType.REFRESH_TOKEN);
 
-        return jwtUtils.generateRefreshToken(claims, loginEntry.getUsername());
+        return jwtUtils.generateRefreshToken(claims, user.getUsername());
     }
 
-    public String buildIdentityToken(LoginEntry loginEntry) {
+    public String buildIdentityToken(UserEntity user) {
         Map<String, Object> claims =
                 Map.of(
-                        "username", loginEntry.getUsername(),
-                        "user", loginEntry.getUser(),
+                        "username", user.getUsername(),
+                        "user",
+                                UserEntity.builder()
+                                        .uuid(user.getUuid())
+                                        .username(user.getUsername())
+                                        .build(),
                         "tokenType", JwtTokenBody.TokenType.IDENTITY_TOKEN);
 
-        return jwtUtils.generateIdentityToken(claims, loginEntry.getUsername());
+        return jwtUtils.generateIdentityToken(claims, user.getUsername());
     }
 }
