1e13be8bee61d0f58976cb47fe4a3716d72cc4dd
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-12-16 10:39:42 +0100

    chore: fix lint

--- src/main/java/com/magentamause/cosybackend/dtos/actiondtos/GameServerCreationDto.java
+++ src/main/java/com/magentamause/cosybackend/dtos/actiondtos/GameServerCreationDto.java
@@ -7,8 +7,6 @@ import com.magentamause.cosybackend.entities.utility.EnvironmentVariableConfigur
 import com.magentamause.cosybackend.entities.utility.PortMapping;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.Data;
 
--- src/main/java/com/magentamause/cosybackend/dtos/entitydtos/GameServerDto.java
+++ src/main/java/com/magentamause/cosybackend/dtos/entitydtos/GameServerDto.java
@@ -9,7 +9,6 @@ import com.magentamause.cosybackend.entities.utility.PortMapping;
 import com.magentamause.cosybackend.entities.utility.VolumeMountConfiguration;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
